4e19be85cdb80e448a48875829cd13fdeb4214b9
Cosmin <cirleacosmin@gmail.com>
2019-10-24 21:38:22 +0300

    added Firebase Auth

--- app/src/main/java/com/example/licenta/Activities/LoginActivity.java
+++ app/src/main/java/com/example/licenta/Activities/LoginActivity.java
@@ -6,13 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.licenta.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
     }
 
     public void OnSignIn(View view) {
